4622d3e987a42f28939fa43466236a8f833ce9c3
thiru <vengadam2001@gamil.com>
2020-07-15 23:26:01 -0700

    reeors commented

--- sample1.java
+++ sample1.java
@@ -1,61 +1,60 @@
-/*data types in a class:
-member variable-variables in a class (int a =0; here a is a variable)
-member function -functions in a class(the functions declared in a class)
-member class- class which is declared in same class
+// /*data types in a class:
+// member variable-variables in a class (int a =0; here a is a variable)
+// member function -functions in a class(the functions declared in a class)
+// member class- class which is declared in same class
  
-class is the blue print of the program
-*/
-/*
-class sample1{
-int a;
-int b=20;
-int c=b+a;
-public static void main(String agrs[])
-{
-sample1 s=new sample1();
-sample1 s1=new sample1();
+// class is the blue print of the program
+// */
+// /*
+// class sample1{
+// int a;
+// int b=20;
+// int c=b+a;
+// public static void main(String agrs[])
+// {
+// sample1 s=new sample1();
+// sample1 s1=new sample1();
 
-s.a=10;
-s1.a=11;
-System.out.println(s.a);
-System.out.println(s1.a);
-System.out.println(s.b);
-System.out.println(s.c+s.b);
-System.out.println(s.c);
+// s.a=10;
+// s1.a=11;
+// System.out.println(s.a);
+// System.out.println(s1.a);
+// System.out.println(s.b);
+// System.out.println(s.c+s.b);
+// System.out.println(s.c);
 
-}}
-*/
+// }}
+// */
 
-import java.util.Arrays;
-import java.util.Scanner;
-class sample1
-{
+// import java.util.Arrays;
+// import java.util.Scanner;
+// class sample1
+// {
 
-String str="";
-int roll=0;
-public static void main(String agrs[])
-{
+// String str="";
+// int roll=0;
+// public static void main(String agrs[])
+// {
 
-Scanner input =new Scanner(System.in);
-System.out.println("enter '!' to stop entering");
-sample1 s[] = new sample1()[1];
-int length=0;
-while(input.next().charAt(0)!='!'||input.next().charAt(0)!="!")
-{
-length++;
-s.copyOf(sample1 s,length);
-s[length].str=input.next();
-}
-while(length>-1)
-{
-	System.out.println(s[length].roll);
-	System.out.println(s[length].str);
-}
-}
-}
+// Scanner input =new Scanner(System.in);
+// System.out.println("enter '!' to stop entering");
+// sample1[] s= new sample1[10];
+// int length=1;
+// while(input.next().charAt(0)!='!')
+// {
+// 	s[length-1].str=input.next();
+// 	s=Arrays.copyOf(s,length++);
+// }
+// while(length>-1)
+// {
+// 	System.out.println(s[length].roll);
+// 	System.out.println(s[length].str);
+// }
+// }
+// }
 
 
-/*
-	USING face book as an example click to view your friend list ...
-	every one has a name and friend to disp their name
-*/
+// /*
+// 	USING face book as an example click to view your friend list ...
+// 	every one has a name and friend to disp their name
+// */
