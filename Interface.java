198ff0be92218d3fec3bf662bb2974e8759fe1c3
thiru <vengadam2001@gamil.com>
2020-07-16 16:21:09 +0530

    interface with multiple inhreitance

--- Interface.java
+++ Interface.java
@@ -1,10 +1,18 @@
 //in interface we can only use abstract method only 
-interface g{
+interface  g{
 abstract void run2();
 void show(); //even with out the abstract keyword also it wll take it as abstract if it ends with semicolon ';'
 }
 
-public class Interface implements g{
+interface h{
+void display();    
+}
+public class Interface implements g,h{
+    @Override
+    public void display() {
+        // TODO Auto-generated method stub
+        System.out.println("displaying");
+    }
     @Override
     public void run2() {
         // TODO Auto-generated method stub
@@ -15,9 +23,10 @@ public class Interface implements g{
         // TODO Auto-generated method stub
         System.out.println("showing");
     }
-    public static void main(String[] args) {
+    public static void main(String args[]) {
     Interface a =new Interface();
         a.run2();
         a.show();
+        a.display();
     }
 }
